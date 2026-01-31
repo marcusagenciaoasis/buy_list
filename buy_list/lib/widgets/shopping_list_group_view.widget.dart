46b35e0cde21a766b02d01bc2b0d1f4287caa7e3
Marcus Vinicius Nascimento Ferreira <247176428+marcusagenciaoasis@users.noreply.github.com>
2026-01-31 15:07:29 -0300

    feat: add ShoppingListGroupView widget for displaying a group of shopping lists

---
+++ buy_list/lib/widgets/shopping_list_group_view.widget.dart
@@ -0,0 +1,13 @@
+import 'package:buy_list/models/shopping_list_group.model.dart';
+import 'package:flutter/material.dart';
+
+class ShoppingListGroupView extends StatelessWidget {
+  const ShoppingListGroupView({super.key, required this.shoppingLists});
+
+  final List<ShoppingListGroup> shoppingLists;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
