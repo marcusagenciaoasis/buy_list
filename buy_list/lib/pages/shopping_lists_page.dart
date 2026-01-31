2417639159aa54d2c7a711d9a2017ff11f1e6328
Marcus Vinicius Nascimento Ferreira <247176428+marcusagenciaoasis@users.noreply.github.com>
2026-01-31 12:42:26 -0300

    feat: implement ShoppingListPage layout with AppBar and SafeArea

--- buy_list/lib/pages/shopping_lists_page.dart
+++ buy_list/lib/pages/shopping_lists_page.dart
@@ -10,6 +10,22 @@ class ShoppingListPage extends StatefulWidget {
 class _ShoppingListPageState extends State<ShoppingListPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        key: Key("appBarTitle"),
+        title: Text('Minhas listas', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+        elevation: 1,
+        backgroundColor: Color(0xFF4CAF50),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.diamond),
+            color: Color(0xFFFFC107),
+          ),
+        ],
+      ),
+      body: SafeArea(child: Column(children: [])),
+    );
   }
 }
