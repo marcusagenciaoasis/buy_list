28b3bc925d100111e252ead3138ee9b61c64b656
Marcus Vinicius Nascimento Ferreira <marcus@agenciaoasis.com.br>
2026-01-31 12:23:04 -0300

    feat: implement ShoppingListPage with basic structure

---
+++ buy_list/lib/pages/shopping_lists_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoppingListPage extends StatefulWidget {
+  const ShoppingListPage({super.key});
+
+  @override
+  State<ShoppingListPage> createState() => _ShoppingListPageState();
+}
+
+class _ShoppingListPageState extends State<ShoppingListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
