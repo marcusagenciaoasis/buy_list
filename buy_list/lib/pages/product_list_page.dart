895cc15c56c9700a8b30b9be130cedc0026c8c59
Marcus Vinicius Nascimento Ferreira <247176428+marcusagenciaoasis@users.noreply.github.com>
2026-01-31 18:54:16 -0300

    feat: implement ProductListPage with app bar and floating action button

--- buy_list/lib/pages/product_list_page.dart
+++ buy_list/lib/pages/product_list_page.dart
@@ -10,6 +10,35 @@ class ProductListPage extends StatefulWidget {
 class _ProductListPageState extends State<ProductListPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF4CAF50),
+        foregroundColor: Colors.white,
+        elevation: 1,
+        leading: IconButton(
+          key: Key("backBtn"),
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          TextButton(
+            key: Key("updateListBtn"),
+            onPressed: () {},
+            style: TextButton.styleFrom(foregroundColor: Colors.white),
+            child: const Text('Atualizar'),
+          ),
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton.extended(
+        key: Key("addNewItemBtn"),
+        onPressed: () {},
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(600)),
+        backgroundColor: const Color(0xFF2195F2),
+        label: const Text('Adicionar', style: TextStyle(color: Colors.white)),
+      ),
+    );
   }
 }
