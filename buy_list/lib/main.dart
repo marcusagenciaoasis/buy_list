775110463d06d8d8384765db48479dcc4cc81253
Marcus Vinicius Nascimento Ferreira <marcus@agenciaoasis.com.br>
2026-01-31 12:30:08 -0300

    feat: remove unused MyHomePage class and ensure debug banner is hidden

--- buy_list/lib/main.dart
+++ buy_list/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shopping List',
+      debugShowCheckedModeBanner: false,
       routes: {
         '/shopping_lists': (context) => const ShoppingListPage(),
         '/create_shopping_list': (context) => const CreateShoppingListPage(),
@@ -21,50 +22,6 @@ class MyApp extends StatelessWidget {
       },
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       initialRoute: '/shopping_lists',
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
